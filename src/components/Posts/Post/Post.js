5bfeb3b7d30adfbf86b094cad763eeab10c7b488
jeezy1052@gmail.com <jeezy1052@gmail.com>
2022-01-28 13:33:23 -0800

    i just got the store set up

---
+++ src/components/Posts/Post/Post.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import useStyles from './styles'
+
+const Post = () => {
+  const classes = useStyles();
+  return (
+    <>
+        
+    </>
+  )
+}
+
+export default Post
